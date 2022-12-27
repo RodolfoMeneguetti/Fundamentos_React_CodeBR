5c0dd57cbfe1d48f1336b822cf4339eeb763da86
RodolfoMeneguetti <paulo.meneguettitec@gmail.com>
2022-12-27 00:34:19 -0300

    Created method  DOM

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import ReactDOM  from "react-dom";
+import React from 'react';
+
+
+const tag =  <strong>Hello Word</strong>
+
+ReactDOM.render(
+  <div>
+      {tag}
+  </div>,
+  document.getElementById('root')
+)
